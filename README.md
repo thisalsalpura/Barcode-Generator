d434050050082f132f4bef1d736fd3cde1957801
thisalsalpura <salpurathisal@gmail.com>
2025-09-03 22:50:40 +0530

    Update 1.0

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 This is a Barcode Generator Project.
-
-<img src="./screenshot_7.png" width="600" height="260" />
-<br />
-
-<img src="./screenshot_8.png" width="600" height="260" />
-<br />
--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.getElementById("btn-genarate").addEventListener('click', function() {
-
-
-    document.getElementById("downloadbtn").style.display = "block";
-
+document.getElementById("btn-genarate").addEventListener('click', function () {
     let val = document.getElementById("txt-input").value;
-    JsBarcode("#barcode", val);
+    if (val !== "") {
+        JsBarcode("#barcode", val);
+        document.getElementById("valueEmpty").style.display = "none";
+        document.getElementById("downloadBtn").style.display = "block";
+    } else {
+        document.getElementById("downloadBtn").style.display = "none";
+        document.getElementById("valueEmpty").style.display = "block";
+    }
 });
 
 
 function downloadSVG() {
     const svg = document.getElementById('barcode').outerHTML;
-    const blob = new Blob([svg], {type: 'image/svg+xml'});
+    const blob = new Blob([svg], { type: 'image/svg+xml' });
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
